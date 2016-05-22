180b02e71aa35e2c1f3bb81bd63ecc5b5869aac7
yepengxj <yepeng_xj@163.com>
2016-05-22 18:31:18 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,15 +3,10 @@
 if [[ ${DBBSI} ]]; then
     eval export SQLALCHEMY_DATABASE_URI=\$BSI_${DBBSI}_URI
     sed -i 's^#DBBSI#^'$SQLALCHEMY_DATABASE_URI'^' /home/caravel/caravel_config.py
-    echo "SQLALCHEMY_DATABASE_URI:"${SQLALCHEMY_DATABASE_URI}
     /usr/local/bin/fabmanager create-admin --app caravel < admin.config
-    echo "SQLALCHEMY_DATABASE_URI:"${SQLALCHEMY_DATABASE_URI}
     caravel db upgrade
-    echo "SQLALCHEMY_DATABASE_URI:"${SQLALCHEMY_DATABASE_URI}
     caravel init
-    echo "SQLALCHEMY_DATABASE_URI:"${SQLALCHEMY_DATABASE_URI}
     #caravel load_examples
-    echo "SQLALCHEMY_DATABASE_URI:"${SQLALCHEMY_DATABASE_URI}
 fi
 echo "SQLALCHEMY_DATABASE_URI:"${SQLALCHEMY_DATABASE_URI}
 caravel runserver -d
