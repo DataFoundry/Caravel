abb7dc0b4615acc8f9374da07979f0b825365215
yepengxj <yepeng_xj@163.com>
2016-05-22 10:02:21 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,10 +3,10 @@
 if [[ ${DBBSI} ]]; then
     eval SQLALCHEMY_DATABASE_URI=\$BSI_${SPARKBSI}_URI
     export SQLALCHEMY_DATABASE_URI
+    /usr/local/bin/fabmanager create-admin --app caravel < admin.config
+    caravel db upgrade
+    caravel init
+    caravel load_examples
 fi
 
-/usr/local/bin/fabmanager create-admin --app caravel < admin.config
-caravel db upgrade
-caravel init
-caravel load_examples
 caravel runserver -d
